c3a40f89a48e5bd6244dc86303311db3c68b5246
Ieven <260343093@qq.com>
2019-11-11 13:56:26 +0800

    我就试试，看看显示什么

--- src/Java/Test.java
+++ src/Java/Test.java
@@ -2,6 +2,7 @@ package Java;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("dddd");
+        System.out.println("OK");
+        System.out.println("Test");
     }
 }
